5dcbf14252efd801c1b60cfbc47c894cbf97973b
anyars-encarta <anyarsencarta@gmail.com>
2024-04-15 18:57:09 +0000

    Implement loading for feed

--- api/index.js
+++ api/index.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/user/1", (req, res) => {
-    res.json(user);
+    setTimeout(() => {
+        res.json(user);
+    }, 3000);
 });
 
 app.get("/api/videos/1", (req, res) => {
-    res.json(videos);
+    setTimeout(() => {
+        res.json(videos);
+    }, 5000);
 });
 
 app.listen(8800, () => {
