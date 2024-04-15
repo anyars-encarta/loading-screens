840b0e023916cfadc798f2cbeebe3e3bf7e89969
anyars-encarta <anyarsencarta@gmail.com>
2024-04-15 17:36:54 +0000

    Install material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Feed from './components/feed/Feed';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get("/api/user/1");
+      setUser(response.data);
+    } catch (e) {
+      console.log(e);
+    };
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  console.log(user);
   return (
     <div className="container">
-      <Sidebar />
+      <Sidebar loading={loading} />
 
       <div className='home'>
-        <Topbar />
+        <Topbar loading={loading} user={user} />
         <Feed />
       </div>
     </div>
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import './topbar.css';
+import React from "react";
+import "./topbar.css";
+import {
+  Mic,
+  Search,
+  VideoCall,
+  Apps,
+  Notifications,
+} from "@material-ui/icons";
+// import Skeleton from "../skeleton/Skeleton";
 
-const Topbar = () => {
+const Topbar = ({ loading, user }) => {
   return (
-    <div className='topbar'>Topbar</div>
-  )
-}
+    <div className="topbar">
+      <div className="topLeft">
+        <img className="logo" src="https://i.ibb.co/KDtnJVY/logo.png" alt="" />
+        <span className="logoText">EncartaTube</span>
+      </div>
+      <div className="topCenter">
+        <div className="topSearch">
+          <input type="text" placeholder="Search" />
+          <div className="topSearchIconContainer">
+            <Search className="topSearchIcon" />
+          </div>
+          <Mic />
+        </div>
+      </div>
+
+      <div className="topRight">
+        <VideoCall className="topIcon" />
+        <Apps className="topIcon" />
+        <Notifications className="topIcon" />
+        <img className="topImg" src={user.avatar} alt="" />
+      </div>
+      {/* {loading ? (
+        <Skeleton type="top" />
+      ) : (
+        
+      )} */}
+    </div>
+  );
+};
 
 export default Topbar;
